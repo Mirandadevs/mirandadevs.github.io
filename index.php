3bbeef8615040160da34e3dc707fd59080b5f31e
Mirandadevs <11243955+Mirandadevs@users.noreply.github.com>
2025-04-18 18:13:55 -0700

    index.php upload

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="propeller" content="a14a499c812085b5e519f55dcba81142">
+    <meta name="title" content="#1 Daily Meditations for Recovering Addicts - Self Help *Just for today* NA">
+    <title>#1 Daily Meditations for Recovering Addicts - Self Help *Just for today* NA</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-4488409638930954",
+    enable_page_level_ads: true
+  });
+</script>
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav id="siteNav" class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Logo and responsive toggle -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#top">
+                	<span class="glyphicon glyphicon-leaf"></span>
+                	MIRANDA DEVS
+                </a>
+            </div>
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active">
+                        <a href="#top">Home</a>
+                    </li>
+                    <!--li>
+                        <a href="#">Products</a>
+                    </li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
+						<ul class="dropdown-menu" aria-labelledby="about-us">
+							<li><a href="#">Engage</a></li>
+							<li><a href="#">Pontificate</a></li>
+							<li><a href="#">Synergize</a></li>
+						</ul>
+					</li-->
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+                
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container -->
+    </nav>
+
+	<!-- Header -->
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1>Self Help</h1>
+                <p>Daily Meditations for Recovering Addicts</p>
+            </div>
+        </div>
+    </header>
+
+	<!-- Intro Section -->
+    <section class="intro">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                	<span class="glyphicon glyphicon-star" style="font-size: 50px"></span><span class="glyphicon glyphicon-star" style="font-size: 50px"></span><span class="glyphicon glyphicon-star" style="font-size: 50px"></span><span class="glyphicon glyphicon-star" style="font-size: 50px"></span><span class="glyphicon glyphicon-star" style="font-size: 50px"></span>
+                    <h2 class="section-heading">5 Stars</h2>
+                    <p class="text-light">Self Help app is one of the anonymous Recovery readings for Android</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+	<!-- Content 1 -->
+    <section class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <img class="img-responsive img-circle center-block" src="images/iphone.jpg" alt="">
+                </div>
+                <div class="col-sm-6">
+                	<h2 class="section-header">Read Anywhere You Go</h2>
+                	<p class="lead text-muted">This app works offline – you do not need an internet connection.<br>It's easy to use, and it'll remind you if you forget to read the days passage</p>
+                	<!--a href="http://bit.ly/jftdownload" target="_blank" class="btn btn-primary btn-lg">Screenshots</a-->
+                </div>                
+                
+            </div>
+        </div>
+    </section>
+
+	<!-- Content 2 -->
+     <section class="content content-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                	<h2 class="section-header">Mltilingual</h2>
+                	<p class="lead text-light">
+			Languages:
+			<br>
+			Arabic (العربية)
+			<br>
+			English
+			<br>
+			French (Français)
+			<br>
+			Japanese (日本語)
+			<br>
+			Portuguese (Português)
+			<br>
+			Russian (Русский)
+			<br>
+			Spanish (Español)
+                	<!--a href="#" class="btn btn-default btn-lg">Test It</a-->
+                </div>    
+                <div class="col-sm-6">
+                    <img class="img-responsive img-circle center-block" src="images/microphone.jpg" alt="">
+                </div>            
+                
+            </div>
+        </div>
+    </section>    
+
+    <!-- Promos -->
+	<div class="container-fluid">
+        <div class="row promo">
+        	<!--a href="#"-->
+				<div class="col-md-4 promo-item item-1">
+					<h3>
+						Helpful
+					</h3>
+				</div>
+            <!--/a>
+            <a href="#"-->
+				<div class="col-md-4 promo-item item-2">
+					<h3>
+						Multilingual
+					</h3>
+				</div>
+            <!--/a>
+			
+			<a href="#"-->
+				<div class="col-md-4 promo-item item-3">
+					<h3>
+						Easy to use
+					</h3>
+				</div>
+            </a>
+        </div>
+    </div><!-- /.container-fluid -->
+
+	<!-- Content 3 -->
+     <section class="content content-3">
+        <div class="container">
+			<h2 class="section-header"><span class="glyphicon glyphicon-heartbeat text-primary"></span><br> Self Help *Just for today* NA</h2>
+			<p class="lead text-muted">Self Help is an offline multilingual application developed in the hope that more people can make the journey from addiction to recovery.<br>It also aims to share a message of Recovery and Hope. It opens to the current day's meditation with any of the other 364 at your fingertips.</p> 
+                    <a href="/jft.apk" target="_blank" class="btn btn-primary btn-lg">Download Now</a>               
+            </div>
+        </div>
+    </section>
+    
+	<!-- Footer -->
+    <footer class="page-footer" id="contact">
+    
+    	<!-- Contact Us -->
+        <div class="contact">
+        	<div class="container">
+				<h2 class="section-heading">Contact Us</h2>
+				<!--p><span class="glyphicon glyphicon-earphone"></span><br> +90 (539) 216 8732 expired</p-->
+				<p><span class="glyphicon glyphicon-envelope"></span><br> mirandadevs [at] gmail [dot] com</p>
+        	</div>
+        </div>
+        	
+        <!-- Copyright etc -->
+        <div class="small-print">
+        	<div class="container">
+        		<!--p>2014 - 2018</p-->
+        	</div>
+        </div>
+        
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    
+    <!-- Custom Javascript -->
+    <script src="js/custom.js"></script>
+    
+    <!-- Piwik -->
+    <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+    var u="//mirandadevs.com/analytics/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+    </script>
+    <noscript><p><img src="//mirandadevs.com/analytics/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Piwik Code -->
+
+</body>
+
+</html>
